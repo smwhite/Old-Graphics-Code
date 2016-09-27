93e41d26d8cbc40ee44d94de902a7b96f567af28
Shawn <smwhite@nevada.unr.edu>
2016-09-27 09:37:54 -0700

    made PA4 and obj file

---
+++ PA4/src/engine.cpp
@@ -0,0 +1,201 @@
+
+#include "engine.h"
+
+Engine::Engine(string name, int width, int height)
+{
+  m_WINDOW_NAME = name;
+  m_WINDOW_WIDTH = width;
+  m_WINDOW_HEIGHT = height;
+  m_FULLSCREEN = false;
+  m_ROTATION_FLIP = true;
+  m_TRANSLATION_FLIP = true;
+  m_PAUSE = 0;
+}
+
+Engine::Engine(string name)
+{
+  m_WINDOW_NAME = name;
+  m_WINDOW_HEIGHT = 0;
+  m_WINDOW_WIDTH = 0;
+  m_FULLSCREEN = true;
+  m_ROTATION_FLIP = true;
+  m_TRANSLATION_FLIP = true;
+  m_PAUSE = 0;
+}
+
+Engine::~Engine()
+{
+  delete m_window;
+  delete m_graphics;
+  m_window = NULL;
+  m_graphics = NULL;
+}
+
+bool Engine::Initialize()
+{
+  // Start a window
+  m_window = new Window();
+  if(!m_window->Initialize(m_WINDOW_NAME, &m_WINDOW_WIDTH, &m_WINDOW_HEIGHT))
+  {
+    printf("The window failed to initialize.\n");
+    return false;
+  }
+
+  // Start the graphics
+  m_graphics = new Graphics();
+  if(!m_graphics->Initialize(m_WINDOW_WIDTH, m_WINDOW_HEIGHT))
+  {
+    printf("The graphics failed to initialize.\n");
+    return false;
+  }
+
+  // Set the time
+  m_currentTimeMillis = GetCurrentTimeMillis();
+
+  // No errors
+  return true;
+}
+
+void Engine::Run()
+{
+  m_running = true;
+
+  while(m_running)
+  {
+    // Update the DT
+    m_DT = getDT();
+
+    // Check the keyboard input
+    while(SDL_PollEvent(&m_event) != 0)
+    {
+      Keyboard();
+
+      Mouse();
+    }
+
+    // Update and render the graphics
+    m_graphics->Update(m_DT,m_ROTATION_FLIP,m_TRANSLATION_FLIP,m_PAUSE,m_ROTATION_FLIP_MOON,m_TRANSLATION_FLIP_MOON,m_PAUSE_MOON);
+    m_graphics->Render();
+
+    // Swap to the Window
+    m_window->Swap();
+  }
+}
+
+void Engine::Keyboard()
+{
+  if(m_event.type == SDL_QUIT)
+  {
+    m_running = false;
+  }
+  else if (m_event.type == SDL_KEYDOWN)
+  {
+
+    // handle key down events here
+    
+	switch(m_event.key.keysym.sym)
+	{
+	  case SDLK_ESCAPE:
+	    m_running = false;
+	    break;
+
+	  case SDLK_a: 
+		m_TRANSLATION_FLIP = !m_TRANSLATION_FLIP; 
+	    break; 
+	
+	  case SDLK_s: 
+		m_PAUSE = 2;
+	    break;
+
+	  case SDLK_d: 
+		m_PAUSE = 3;
+	    break;
+
+	  case SDLK_z: 
+	    m_PAUSE = 1;
+	    break;
+
+	  case SDLK_x: 
+	    m_PAUSE = 0;
+	    break;
+
+      case SDLK_LEFT: 
+		m_TRANSLATION_FLIP = false; 
+	    break; 
+
+      case SDLK_RIGHT: 
+		m_TRANSLATION_FLIP = true; 
+	    break;
+
+      case SDLK_g: 
+		m_ROTATION_FLIP_MOON = false; 
+	    break; 
+
+      case SDLK_h: 
+		m_ROTATION_FLIP_MOON = true; 
+	    break;
+
+      case SDLK_n: 
+		m_TRANSLATION_FLIP_MOON = false; 
+	    break; 
+
+      case SDLK_m: 
+		m_TRANSLATION_FLIP_MOON = true; 
+	    break;
+
+	  case SDLK_b: 
+		m_PAUSE_MOON = 2;
+	    break;
+
+	  case SDLK_v: 
+		m_PAUSE_MOON = 3;
+	    break;
+
+      case SDLK_j: 
+		m_PAUSE_MOON = 4;
+	    break;
+
+	  case SDLK_k: 
+		m_PAUSE_MOON = 5;
+	    break;
+
+	}
+  }
+}
+
+void Engine::Mouse()
+{
+if (m_event.type == SDL_MOUSEBUTTONDOWN)
+  {
+    // handle mouse events here
+    if(m_event.button.button == SDL_BUTTON_LEFT)
+    {
+      m_ROTATION_FLIP = !m_ROTATION_FLIP;
+    }
+	if(m_event.button.button == SDL_BUTTON_RIGHT)
+	{
+	  m_PAUSE = 4;
+	}
+	if(m_event.button.button == SDL_BUTTON_MIDDLE)
+	{
+	  m_PAUSE = 5;
+	}
+  }
+
+}
+unsigned int Engine::getDT()
+{
+  long long TimeNowMillis = GetCurrentTimeMillis();
+  assert(TimeNowMillis >= m_currentTimeMillis);
+  unsigned int DeltaTimeMillis = (unsigned int)(TimeNowMillis - m_currentTimeMillis);
+  m_currentTimeMillis = TimeNowMillis;
+  return DeltaTimeMillis;
+}
+
+long long Engine::GetCurrentTimeMillis()
+{
+  timeval t;
+  gettimeofday(&t, NULL);
+  long long ret = t.tv_sec * 1000 + t.tv_usec / 1000;
+  return ret;
+}
---
+++ PA4/src/object.cpp
@@ -0,0 +1,166 @@
+#include "object.h"
+#include <iostream>
+
+Object::Object(glm::mat4 center)
+{  
+  /*
+    # Blender File for my object. Added for trouble shooting
+    o Cube
+	v 2.132345 -1.000000 -0.297700
+	v 2.132345 -1.000000 1.000000
+	v -1.000000 -1.000000 1.000000
+	v -1.000000 -1.000000 -0.297700
+	v 2.132346 -0.579089 -0.297700
+	v 2.132344 -0.579089 1.000001
+	v -1.000000 -0.579089 1.000000
+	v -1.000000 -0.579089 -0.297700
+    s off
+	f 5 2 1
+	f 6 3 2
+	f 7 4 3
+	f 1 8 5
+	f 5 6 2
+	f 6 7 3
+	f 7 8 4
+	f 1 4 8
+	f 2 4 1
+	f 2 3 4
+  */
+
+  Vertices = {
+    {{1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 0.0f}},
+    {{1.0f, -1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
+    {{-1.0f, -1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+    {{-1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 1.0f}},
+    {{1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 0.0f}},
+    {{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}},
+    {{-1.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 1.0f}},
+    {{-1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 1.0f}}
+  };
+
+  Indices = {
+    2, 3, 4,
+    8, 7, 6,
+    1, 5, 6,
+    2, 6, 7,
+    7, 8, 4,
+    1, 4, 8,
+    1, 2, 4,
+    5, 8, 6,
+    2, 1, 6,
+    3, 2, 7,
+    3, 7, 4,
+    5, 1, 8
+  };
+
+  // The index works at a 0th index
+  for(unsigned int i = 0; i < Indices.size(); i++)
+  {
+    Indices[i] = Indices[i] - 1;
+  }
+
+  angle = 0.0f;
+
+  glGenBuffers(1, &VB);
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(), &Vertices[0], GL_STATIC_DRAW);
+
+  glGenBuffers(1, &IB);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.size(), &Indices[0], GL_STATIC_DRAW);
+
+  centerOfOrbit = center;
+  location= glm::mat4(1.0f);
+
+}
+
+Object::~Object()
+{
+  Vertices.clear();
+  Indices.clear();
+}
+
+void Object::Update(unsigned int dt,bool rotation,bool translation, int pause,glm::mat4 center,float scale)
+{
+  angle += dt * M_PI/1000;
+
+  switch(pause)
+  {
+    case 0:
+	  rateR=rateT=M_PI/1000;
+	  break;
+	case 1:
+	  rateR=rateT=0;
+	  break;
+	case 2:
+        rateT = M_PI/1000;
+	  break;
+	case 3:
+        rateT = 0;
+	  break;
+	case 4:
+		rateR = M_PI/1000;
+	  break;
+	case 5:
+		rateR = 0;
+	  break;
+  }
+
+  if(rotation)
+  {
+    angleR += dt * rateR;
+  }
+  else if(!rotation)
+  {
+    angleR -= dt * rateR;
+  }
+
+  glm::mat4 rot= glm::rotate(glm::mat4(1.0f), (angleR), glm::vec3(0.0, 1.0, 0.0));
+
+  if(translation)
+  {
+    angleT += dt * rateT;
+  }
+  else if(!translation)
+  {
+    angleT -= dt * rateT;
+  }
+
+  glm::vec3 circle(5.0f * cos(angleT), 1.0f, 5.0f * sin(angleT));
+
+
+
+  glm::mat4 trans= glm::translate(center,circle);
+
+  location = trans;
+
+  trans = glm::scale(trans,glm::vec3(scale,scale,scale));
+  model= trans*rot;
+}
+
+glm::mat4 Object::GetModel()
+{
+  return model;
+}
+
+void Object::Render()
+{
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex,color));
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+
+  glDrawElements(GL_TRIANGLES, Indices.size(), GL_UNSIGNED_INT, 0);
+
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+}
+
+glm::mat4 Object::GetLocation()
+{
+  return location;
+}
